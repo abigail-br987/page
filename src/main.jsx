ee0d3597dd940c7b3effb8cb2a519e4e7e09bd6b
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-06-27 14:46:12 -0500

    gmr

--- src/main.jsx
+++ src/main.jsx
@@ -1,53 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import './game.css'
-import Home from "./pages/Home"
-import Resources from "./pages/Resources"
-import Games from "./pages/Games"
-
-import App from "./App"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import './game.css';
+import Home from './pages/Home';
+import Resources from './pages/Resources';
+import Games from './pages/Games';
 import {
-  createBrowserRouter,
-  RouterProvider,
+  HashRouter as Router,
+  Routes,
   Route
-} from "react-router-dom"
+} from 'react-router-dom';
 
-const router=createBrowserRouter([
-  {
-    path: "/",
-    element: <Home></Home>
-  },
-  {
-    path: "recursos",
-    element: <Resources></Resources>
-  },
-  {
-    path: "juegos",
-    element: <Games></Games>
-  },
-  {
-    path: "home",
-    element: <Home></Home>
-  },
-  {
-    path: "foreveryoung",
-    element: <Home></Home>
-  },
-  {
-    path: "*",
-    element: <Home></Home>
-  },
-  {
-    path: "foreveryoung/home",
-    element: <Home></Home>
-  },
-  
+const router = (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/recursos" element={<Resources />} />
+      <Route path="/juegos" element={<Games />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/foreveryoung" element={<Home />} />
+      <Route path="/foreveryoung/home" element={<Home />} />
+      <Route path="*" element={<Home />} />
+    </Routes>
+  </Router>
+);
 
-])
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <RouterProvider router={router} />
-
-)
+ReactDOM.render(router, document.getElementById('root'));
