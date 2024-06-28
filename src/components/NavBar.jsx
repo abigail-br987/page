5d3532fa73489bdc94dd634d2afa9948bb514edd
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-06-27 20:22:00 -0500

    hola

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,7 +8,7 @@ function NavBar() {
                 <ul>
                     <li><a href="https://www.instagram.com/forever_young_voluntariado/" target="_blank">Instagram</a></li>
                     <li><Link to="/juegos">Juegos</Link></li>
-                    <li><Link to= "/home">  <img src="public/logo-foreveryoung.png" alt="Company Logo"></img> </Link></li>
+                    <li><Link to= "/home">  <img src="src/assets/logo-foreveryoung.png" alt="Company Logo"></img> </Link></li>
                     <li><Link to="/recursos">Recursos</Link></li>
                     <li><a href="https://drive.google.com/drive/folders/1gNpCWpx-Tmdrjx7DPJAfCOhkjSf6tyLJ?usp=sharing" target="_blank">Diseños</a></li>
                 </ul> 
