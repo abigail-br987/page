70025a8839522aee8c8a82caa6e40dbf7d9cac82
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-06-29 05:49:08 -0500

    change

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -11,7 +11,7 @@ function Section1() {
             </div>
             <div class="right-container">
                 <img class="right-image" src="/logo-foreveryoung2.png" alt="Logo"></img>
-                <p>Somos un grupo de voluntariado de Servicio de Medicina del Adolescente del Hospital del Niño de Breña, Lima, Perú. Nuestro objetivo es apoyar a los adolescentes mediante actividades y promover su salud sexual, integral, mental y física. Fomentamos el amor, comprensión y tolerancia. Además, difundimos información y concientizamos sobre los derechos de los adolescentes y la prevención de actitudes riesgosas.</p>
+                <p> un grupo de voluntariado de Servicio de Medicina del Adolescente del Hospital del Niño de Breña, Lima, Perú. Nuestro objetivo es apoyar a los adolescentes mediante actividades y promover su salud sexual, integral, mental y física. Fomentamos el amor, comprensión y tolerancia. Además, difundimos información y concientizamos sobre los derechos de los adolescentes y la prevención de actitudes riesgosas.</p>
             </div>
         </section>
     )
